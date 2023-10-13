2f86e8586ea7bc70608f8d052cb30d14ec20f3a4
Giant_Salted_Fish <wangzhichuan6@outlook.com>
2023-10-14 00:20:23 +0800

    Update root package qualifier.

---
+++ src/main/java/gsf/fmum/common/load/BuildableType.java
@@ -0,0 +1,43 @@
+package com.fmum.common.load;
+
+import com.fmum.common.pack.IContentPack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Optional;
+
+public abstract class BuildableType
+{
+	protected String name;
+	
+	protected IContentPack from_pack;
+	
+	protected BuildableType() { }
+	
+	public void buildServerSide( IContentBuildContext ctx )
+	{
+		this.name = Optional.ofNullable( this.name )
+			.orElseGet( ctx::fallbackName );
+		this.from_pack = ctx.contentPack();
+	}
+	
+	@SideOnly( Side.CLIENT )
+	public void buildClientSide( IContentBuildContext ctx ) {
+		this.buildServerSide( ctx );
+	}
+	
+	public String name() {
+		return this.name;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return String.format(
+			"%s::<%s.%s>", this._typeHint(),
+			this.from_pack.toString(), this.name
+		);
+	}
+	
+	protected abstract String _typeHint();
+}
